b255b6137a53c6137cafe105d5ebc4887cad5086
minsekim1 <minseung@4dayworks.com>
2022-12-26 23:05:42 +0900

    이름수정 추가

--- react/components/pages/index/HomeHeader.tsx
+++ react/components/pages/index/HomeHeader.tsx
@@ -4,10 +4,18 @@ import { atom, useRecoilState, useRecoilValue } from "recoil";
 import SmsRoundedIcon from "@mui/icons-material/SmsRounded";
 import { Settings } from "@mui/icons-material";
 import { useRouter } from "next/router";
+import { localStorageEffect } from "../../../constant/hook/localStorageEffect";
+import uuid from "react-uuid";
+import { ax } from "../../../pages/_app";
 
 const HomeHeightState = atom<number>({ key: "HomeHeightState", default: 1000 });
 export const IsHomeHeaderState = atom<boolean>({ key: "IsHomeHeaderState", default: false });
 export const IsHideHomeHeaderState = atom<boolean>({ key: "IsHideHomeHeaderState", default: false });
+const userDataState = atom<{ uuid: string; name: string | null }>({
+  key: "userDataState",
+  default: { uuid: uuid(), name: null },
+  effects_UNSTABLE: [localStorageEffect("/cache/userDataState")],
+});
 
 export const HomeHeader = () => {
   const router = useRouter();
@@ -15,7 +23,7 @@ export const HomeHeader = () => {
   const [isOpen, setIsOpen] = useRecoilState(IsHomeHeaderState);
   const isHide = useRecoilValue(IsHideHomeHeaderState);
   const headerRef = useRef<HTMLDivElement>(null);
-
+  const [userData, setUserData] = useRecoilState(userDataState);
   useEffect(() => {
     const handleResize = () => {
       if (headerRef.current) setHeight(Math.max(headerRef.current.clientHeight - 305.88, 0));
@@ -61,17 +69,30 @@ export const HomeHeader = () => {
           <ButtonBase sx={{ ml: 1.5, borderRadius: 4, padding: 1 }} onClick={onClickChat}>
             <SmsRoundedIcon style={{ fontSize: 33, transform: "scaleX(-1)", color: "#CEC2FF" }} />
           </ButtonBase>
-          {/* <ButtonBase style={{ borderRadius: 8, padding: 4, margin: 16 }} onClick={onClickChat}>
-            <SmsRoundedIcon style={{ fontSize: 33, transform: "scaleX(-1)", color: "#CEC2FF" }} />
-          </ButtonBase> */}
+        </div>
+        <div>
+          <input
+            defaultValue={userData.name ? userData.name : undefined}
+            placeholder="이름을 정해주세요."
+            onBlur={(e) => {
+              const name = e.target.value;
+              const isChange = confirm(`${name}로 이름을 변경하시겠습니까?`);
+              if (isChange) {
+                setUserData((prev) => {
+                  return { ...prev, name: name };
+                });
+                if (!userData.name) ax.post(`/user/create`, { name, userId: userData.uuid });
+                else ax.post(`/user/update`, { name, userId: userData.uuid });
+              } else {
+                e.target.value = userData.name ? userData.name : "";
+              }
+            }}
+          ></input>
         </div>
         <div>
           <ButtonBase sx={{ mr: 1.5, borderRadius: 4, padding: 1 }} onClick={onClickSetting}>
             <Settings style={{ fontSize: 33, color: "#CEC2FF" }} />
           </ButtonBase>
-          {/* <ButtonBase style={{ borderRadius: 8, padding: 4, margin: 16 }} onClick={onClickSetting}>
-            <Settings style={{ fontSize: 33, color: "#CEC2FF" }} />
-          </ButtonBase> */}
         </div>
       </div>
       <img src="/images/header_background.svg" width="100%" />
--- react/pages/_app.tsx
+++ react/pages/_app.tsx
@@ -1,7 +1,14 @@
+import axios from "axios";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
 import { RecoilRoot } from "recoil";
 import "/public/styles/globals.css";
+
+export const ax = axios.create({
+  baseURL: "http://3.113.100.47:8080/",
+  timeout: 10000,
+  // headers: { "X-Custom-Header": "foobar" },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
